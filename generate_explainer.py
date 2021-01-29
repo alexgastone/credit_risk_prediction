9482cb4915200a7c577d25076ce58798a291d7ca
Alex <alexandra.gg150@gmail.com>
2021-01-29 12:31:38 +0100

    updated dashboard config

--- generate_explainer.py
+++ generate_explainer.py
@@ -23,18 +23,23 @@ def main():
         "Cnt_Fam_Members": 'Family size'
     }
 
-    explainer = ClassifierExplainer(classifier, X_test, y_test, shap='tree', \
-        cats=[{"Name_Housing_Type":['House/appartment', 'With parents', 'Municipal apartment', 'Rented apartment',\
-        "Office appartment", "Co-op appartment"]},{"Name_Income_Type": ['Working', 'Commercial Associate', \
-            'Pensioner', 'State servant', 'Student']}, {"Name_Education_Type": ['Secondary', 'Higher education', \
-                'Lower secondary', 'Academic degree']}, {"Name_Family_Status": ['Married', 'Single', 'Civil marriage',\
-                    'Separated', 'Widow']}],\
-                        descriptions=feature_descriptions, \
-                            labels=['No loan/paid off', '1-29 days past due', '30-59 days past due', '60-89 days past due', \
-                                '90-119 days past due', '120-149 days past due', 'overdue more than 150 days'], \
+    # cats contains one-hot-encoded vectors
+    explainer = ClassifierExplainer(classifier, X_test, y_test, shap='tree',\ 
+                                    cats=[{"Name_Housing_Type":['Housing_Co-Op Apartment', 'Housing_House / Apartment','Housing_With Parents', 'Housing_Municipal Apartment', 'Housing_Rented Apartment',"Housing_Office Apartment"]},\
+                                        {"Name_Income_Type": ['Income_Working', 'Income_Commercial Associate', 'Income_Pensioner', 'Income_State Servant', 'Income_Student']}, \
+                                        {"Name_Education_Type": ['Education_Secondary / Secondary Special', 'Education_Higher Education', 'Education_Incomplete Higher','Education_Lower Secondary', 'Education_Academic Degree']}, \
+                                        {"Name_Family_Status": ['Family_Married', 'Family_Single / Not Married', 'Family_Civil Marriage','Family_Separated', 'Family_Widow']}],\
+                                    descriptions=feature_descriptions,\
+                                    labels=['No loan/paid off', '1-29 days past due', '30-59 days past due', '60-89 days past due','90-119 days past due', '120-149 days past due', 'overdue more than 150 days'],\
                                     target = "Credit Status")
 
-    db = ExplainerDashboard(explainer, title="Credit Explainer", mode='external')
+    db = ExplainerDashboard(explainer, title="Credit Explainer", mode='external', hide_pdp=True, \
+        importances=True, model_summary=True, contributions=True, whatif=False,\
+            shap_dependence=False, shap_interaction=False, decision_trees=False)
     
     explainer.dump("explainer.joblib")
     db.to_yaml("dashboard.yaml")
+
+
+if __name__ == '__main__':
+    main()
